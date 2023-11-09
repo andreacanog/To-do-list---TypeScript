317164ec7ee791488dbae4c8913176aafa16d626
Andrea Cano <andreacanogisbert@gmail.com>
2023-11-09 15:23:45 -0800

    create handleAdd on App.tsx

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { useState } from 'react';
+import { Todo } from './components/model';
+
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
