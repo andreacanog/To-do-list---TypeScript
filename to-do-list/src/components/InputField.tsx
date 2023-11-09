696ffde97833239fee4a991ad65d41000cd93bbd
Andrea Cano <andreacanogisbert@gmail.com>
2023-11-09 15:22:47 -0800

    create model component

--- to-do-list/src/components/InputField.tsx
+++ to-do-list/src/components/InputField.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import "./style.css";
+import { useState } from 'react';
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: () => void;
+}
 
 
-const InputField = () => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  
+
   return (
-    <form className='input'>
-      <input type="input" placeholder='Enter a task' className='input__box' />
+    <form className='input' onSubmit={handleAdd}>
+      <input type="input" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder='Enter a task' className='input__box' />
       <button className='input_submit' type='submit'>
         Go
       </button>
