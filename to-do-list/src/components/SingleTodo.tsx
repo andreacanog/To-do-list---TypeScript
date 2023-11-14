6c55e4650c1d406b1c95f0f6865249baa4d914bf
Andrea Cano <andreacanogisbert@gmail.com>
2023-11-14 10:07:58 -0800

    implement functionaltity for the delete button

--- to-do-list/src/components/SingleTodo.tsx
+++ to-do-list/src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Todo } from '../model'
 import {AiFillEdit, AiFillDelete} from "react-icons/ai"
 import {MdDone} from "react-icons/md"
@@ -11,9 +11,15 @@ type Props = {
 }
 
 const SingleTodo = ({todo, todos, setTodos}: Props) => {
+    const [edit, setEdit] = useState<boolean>(false)
+    const [editTodo, setEditTodo] = useState<string>(todo.todo)
 
     const handleDone = (id: number) => {
         setTodos(todos.map((todo) => todo.id === id ? {...todo, isDone: !todo.isDone} : todo))
+    }
+
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id))
     }
 
   return (
@@ -27,7 +33,7 @@ const SingleTodo = ({todo, todos, setTodos}: Props) => {
         <span className="icon">
             <AiFillEdit/>
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
             <AiFillDelete/>
         </span>
         <span className="icon" onClick={() => handleDone(todo.id)}> 
